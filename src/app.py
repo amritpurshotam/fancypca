8f23e88ad14ae87e7589d3687a0eadaa5a881e5f
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-08-01 11:32:06 +0200

    Add state

--- src/app.py
+++ src/app.py
@@ -3,47 +3,75 @@ import tensorflow as tf
 
 from fancy_pca import fancy_pca
 
+if "min_alpha" not in st.session_state:
+    st.session_state.min_alpha = -2.0
+
+if "max_alpha" not in st.session_state:
+    st.session_state.max_alpha = 3.0
+
+if "alpha_1" not in st.session_state:
+    st.session_state.alpha_1 = float(tf.random.normal((1,), mean=0, stddev=1))
+
+if "alpha_2" not in st.session_state:
+    st.session_state.alpha_2 = float(tf.random.normal((1,), mean=0, stddev=1))
+
+if "alpha_3" not in st.session_state:
+    st.session_state.alpha_3 = float(tf.random.normal((1,), mean=0, stddev=1))
+
+if "image" not in st.session_state:
+    image = tf.io.read_file("./images/bird.jpg")
+    image = tf.image.decode_jpeg(image, channels=3)
+    st.session_state.image = image
+
+if "pca_image" not in st.session_state:
+    pca_image = fancy_pca(
+        st.session_state.image,
+        [st.session_state.alpha_1, st.session_state.alpha_2, st.session_state.alpha_3],
+    )
+    st.session_state.pca_image = pca_image
+
+
+def augment_image():
+    pca_image = fancy_pca(
+        st.session_state.image,
+        [st.session_state.alpha_1, st.session_state.alpha_2, st.session_state.alpha_3],
+    )
+    st.session_state.pca_image = pca_image
+
+
 st.title("PCA Colour Augmentation")
 st.header("Demo")
-
-alpha_std = st.slider(
-    "Alpha Standard Deviation", min_value=0.0, max_value=1.0, step=0.01, value=0.1
-)
-alphas = tf.random.normal((3,), mean=0, stddev=alpha_std)
-min_alpha = alpha_std * 3 * -1
-max_alpha = alpha_std * 3
-alphas = tf.clip_by_value(alphas, min_alpha, max_alpha)
-col1, col2, col3 = st.beta_columns(3)
-alpha_1 = col1.slider(
-    "Alpha 1",
-    min_value=min_alpha,
-    max_value=max_alpha,
-    step=0.01,
-    value=float(alphas[0]),
-)
-alpha_2 = col2.slider(
-    "Alpha 2",
-    min_value=min_alpha,
-    max_value=max_alpha,
-    step=0.01,
-    value=float(alphas[1]),
-)
-alpha_3 = col3.slider(
-    "Alpha 3",
-    min_value=min_alpha,
-    max_value=max_alpha,
-    step=0.01,
-    value=float(alphas[2]),
-)
-
-image = tf.io.read_file("./images/bird.jpg")
-image = tf.image.decode_jpeg(image, channels=3)
-pca_image = fancy_pca(image, [alpha_1, alpha_2, alpha_3])
 
 col1, col2 = st.beta_columns(2)
 
 col1.subheader("Original")
-col1.image(image.numpy(), use_column_width=True)
+col1.image(st.session_state.image.numpy(), use_column_width=True)
 
 col2.subheader("PCA Colour Augmented")
-col2.image(pca_image.numpy(), use_column_width=True)
+col2.image(st.session_state.pca_image.numpy(), use_column_width=True)
+
+col1, col2, col3 = st.beta_columns(3)
+col1.slider(
+    "Alpha 1",
+    min_value=st.session_state.min_alpha,
+    max_value=st.session_state.max_alpha,
+    step=0.01,
+    key="alpha_1",
+    on_change=augment_image,
+)
+col2.slider(
+    "Alpha 2",
+    min_value=st.session_state.min_alpha,
+    max_value=st.session_state.max_alpha,
+    step=0.01,
+    key="alpha_2",
+    on_change=augment_image,
+)
+col3.slider(
+    "Alpha 3",
+    min_value=st.session_state.min_alpha,
+    max_value=st.session_state.max_alpha,
+    step=0.01,
+    key="alpha_3",
+    on_change=augment_image,
+)
